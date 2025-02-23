2ccd8fbd6a251832ad886b477a73f63dd925abe2
Hugo <hugowinged@gmail.com>
2025-02-22 18:28:07 -0800

    ...

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
+
+const storedBooks = [];
+
 function Book(title, author, pages, bookRead) {
-	this.storedBooks = [];
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -9,22 +11,13 @@ function Book(title, author, pages, bookRead) {
 
 Book.prototype.addBookToLibrary = function() {
     let bookInfo = `Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}, Have read book?: ${this.bookRead}`;
-    this.storedBooks.push(bookInfo);
+    storedBooks.push(bookInfo);
     return bookInfo;
-}
-
-Book.prototype.loopThroughBooks = function() {
-	for(let i=0; i < this.storedBooks.length; i++) {
-  	return this.storedBooks[i];
-	}
 }
 
 const bookOfTruths = new Book('Ass Hat McGee', 'Big Dick', 269, true)
 const bookOfHoes = new Book('Slap A Hoe Twice', 'Charleston White', 500, false)
 
 console.log(bookOfTruths.addBookToLibrary());
-console.log(bookOfTruths.storedBooks);
-console.log(bookOfTruths.loopThroughBooks());
 console.log(bookOfHoes.addBookToLibrary());
-console.log(bookOfHoes.storedBooks);
-console.log(bookOfHoes.loopThroughBooks());
+storedBooks.forEach(book => console.log(book));
